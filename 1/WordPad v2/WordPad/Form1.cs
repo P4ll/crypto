456990f7c18b0a917206ce6ff691b19b90d377c3
P4ll <cornelius.weling@gmail.com>
2020-01-02 19:05:44 +0400

    add tests to lab1

--- 1/WordPad v2/WordPad/Form1.cs
+++ 1/WordPad v2/WordPad/Form1.cs
@@ -10,10 +10,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Reflection;
 
-namespace WordPad
-{
-    public partial class Form1 : Form
-    {
+namespace WordPad {
+    public partial class Form1 : Form {
         private const string DIR_PATH = "\\..\\..\\Files\\";
         private const string STAND_FILE_NAME = "Untitled";
         private string _currentFileName = STAND_FILE_NAME + ".txt";
@@ -27,12 +25,10 @@ namespace WordPad
         private object[] objs;
 
 
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
             _pluginsFileName = Directory.GetFiles("Plugins");
-            for (int i = 0; i < _pluginsFileName.Length; ++i)
-            {
+            for (int i = 0; i < _pluginsFileName.Length; ++i) {
                 _pluginsFileName[i] = Path.GetFullPath(_pluginsFileName[i]);
             }
             int n = _pluginsFileName.Length;
@@ -41,8 +37,7 @@ namespace WordPad
             asms = new Assembly[n];
             types = new Type[n];
             objs = new object[n];
-            for (int i = 0; i < n; ++i)
-            {
+            for (int i = 0; i < n; ++i) {
                 asms[i] = Assembly.LoadFile(_pluginsFileName[i]);
                 types[i] = asms[i].GetType("Plugin.Class1", true, true);
                 objs[i] = Activator.CreateInstance(types[i]);
@@ -50,21 +45,17 @@ namespace WordPad
             }
         }
 
-        private void newToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+        private void newToolStripMenuItem_Click(object sender, EventArgs e) {
             textBox.Text = "";
             _currentFileName = getNewFileName();
             setFrameName(_currentFileName);
         }
 
-        private void openToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) {
             openDialog.FileName = "";
             openDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
-            if (openDialog.ShowDialog() == DialogResult.OK)
-            {
-                using (StreamReader sr = new StreamReader(openDialog.FileName, Encoding.Default))
-                {
+            if (openDialog.ShowDialog() == DialogResult.OK) {
+                using (StreamReader sr = new StreamReader(openDialog.FileName, Encoding.Default)) {
                     textBox.Text = sr.ReadToEnd();
                     _currentFileName = openDialog.FileName;
                     setFrameName(_currentFileName);
@@ -72,63 +63,50 @@ namespace WordPad
             }
         }
 
-        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            using (StreamWriter sw = new StreamWriter(_currentFileName))
-            {
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e) {
+            using (StreamWriter sw = new StreamWriter(_currentFileName)) {
                 sw.Write(textBox.Text);
             }
             _textChanged = false;
         }
 
-        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (saveAsDialog.ShowDialog() == DialogResult.OK)
-            {
-                using (StreamWriter sw = new StreamWriter(_currentFileName = saveAsDialog.FileName))
-                {
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (saveAsDialog.ShowDialog() == DialogResult.OK) {
+                using (StreamWriter sw = new StreamWriter(_currentFileName = saveAsDialog.FileName)) {
                     sw.Write(textBox.Text);
                     setFrameName(_currentFileName);
                 }
             }
-            else
-            {
+            else {
                 _cans = true;
                 return;
             }
             _textChanged = false;
         }
 
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (_textChanged)
-            {
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (_textChanged) {
                 //saveAsToolStripMenuItem_Click(sender, e);
                 Form1.ActiveForm.Close();
             }
-            else
-            {
+            else {
                 Form1.ActiveForm.Close();
             }
         }
 
-        private string getNewFileName()
-        {
+        private string getNewFileName() {
             int i = 0;
-            while (File.Exists(STAND_FILE_NAME + i + ".txt"))
-            {
+            while (File.Exists(STAND_FILE_NAME + i + ".txt")) {
                 i++;
             }
             return STAND_FILE_NAME + i + ".txt";
         }
 
-        private void setFrameName(string str)
-        {
+        private void setFrameName(string str) {
             Form1.ActiveForm.Text = str;
         }
 
-        private void Form1_Shown(object sender, EventArgs e)
-        {
+        private void Form1_Shown(object sender, EventArgs e) {
             setFrameName(_currentFileName = getNewFileName());
             foreach (var i in _pluginNames)
                 pluginsToolStripMenuItem.DropDownItems.Add(i);
@@ -136,55 +114,45 @@ namespace WordPad
                 pluginsToolStripMenuItem.DropDownItems[i].Click += invo;
         }
 
-        private void invo(object sender, EventArgs e)
-        {
+        private void invo(object sender, EventArgs e) {
             var sen = sender as ToolStripMenuItem;
             int index = (sen.OwnerItem as ToolStripMenuItem).DropDownItems.IndexOf(sen);
-            if (_isPluginStart[index])
-            {
+            if (_isPluginStart[index]) {
                 sen.Checked = false;
                 stopPlugin(index);
                 _isPluginStart[index] = false;
             }
-            else
-            {
+            else {
                 sen.Checked = true;
                 startPlugin(index);
                 _isPluginStart[index] = true;
             }
         }
 
-        private void startPlugin(int index)
-        {
+        private void startPlugin(int index) {
             MethodInfo method = types[index].GetMethod("run");
             object result = method.Invoke(objs[index], new object[] { Form1.ActiveForm, textBox });
         }
 
-        private void stopPlugin(int index)
-        {
+        private void stopPlugin(int index) {
             MethodInfo method = types[index].GetMethod("stop");
             object result = method.Invoke(objs[index], new object[] { });
         }
 
-        private void textBox_TextChanged(object sender, EventArgs e)
-        {
+        private void textBox_TextChanged(object sender, EventArgs e) {
             _textChanged = true;
         }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            if (_textChanged)
-            {
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            if (_textChanged) {
                 string mess = "Сохранить изменения?";
                 string capt = "Сохранение";
                 MessageBoxButtons bt = MessageBoxButtons.YesNo;
                 DialogResult = MessageBox.Show(mess, capt, bt);
-                if (DialogResult == DialogResult.Yes)
-                {
+                if (DialogResult == DialogResult.Yes) {
                     saveAsToolStripMenuItem_Click(sender, e);
                 }
-                if (_cans)
-                {
+                if (_cans) {
                     e.Cancel = true;
                     _cans = false;
                 }
--- 1/WordPad v2/crypto-test/Class1.cs
+++ 1/WordPad v2/crypto-test/Class1.cs
@@ -6,10 +6,8 @@ using System.Windows.Forms;
 using Microsoft.VisualBasic;
 using System.Threading.Tasks;
 
-namespace Plugin
-{
-    public class Class1
-    {
+namespace Plugin {
+    public class Class1 {
         public const string Name = "crypto-test";
         private RichTextBox textBox;
         Form form;
@@ -17,8 +15,7 @@ namespace Plugin
         private MenuStrip _menu;
         private string _delims = " .,\t";
 
-        public void run(ref Form form, ref RichTextBox textBox)
-        {
+        public void run(ref Form form, ref RichTextBox textBox) {
             this.textBox = textBox;
             this.form = form;
             _menu = (MenuStrip)form.Controls[1];
@@ -34,17 +31,12 @@ namespace Plugin
         private void generate(object sender, EventArgs e) {
             string result = Interaction.InputBox("Введите длину генерируемой последовательности (10000)");
             int seqLength = 10000;
-            try
-            {
+            try {
                 int tLen = Int32.Parse(result);
                 seqLength = tLen;
             }
-            catch (ArgumentNullException e1)
-            {
-            }
-            catch (FormatException e2)
-            {
-                MessageBox.Show("Неаерный формат");
+            catch (FormatException e2) {
+                MessageBox.Show("Неверное число");
             }
             catch (OverflowException e3) {
                 MessageBox.Show("Переполнение");
@@ -62,41 +54,111 @@ namespace Plugin
         }
 
         private void test(object sender, EventArgs e) {
-            int[] vals = getVals(textBox.Text);
-            for (int i = 0; i < vals.Length; ++i) {
-                vals[i] = 2 * vals[i] - 1;
-            }
+            double bound = 1.82138636;
+            int[] vals = getVals(textBox.Text, true);
             int res = 0;
             foreach (var i in vals) {
-                res += vals[i];
+                res += i;
             }
-            double stat = (double)res / Math.Sqrt(vals.Length);
+            double stat = (double)Math.Abs(res) / Math.Sqrt(vals.Length);
+            if (stat > bound) {
+                MessageBox.Show($"Частотный тест не пройден. Значени статистики {stat}");
+                return;
+            }
+            int[] valsWoTr = getVals(textBox.Text, true);
+            int countOnes = 0;
+            foreach (var i in valsWoTr) {
+                countOnes += i;
+            }
+            double freq = (double)countOnes / valsWoTr.Length;
+            int vn = 1;
+            for (int i = 0; i < valsWoTr.Length - 1; ++i) {
+                if (valsWoTr[i] != valsWoTr[i + 1])
+                    vn++;
+            }
+            double statEqual = Math.Abs((double)vn - 2 * valsWoTr.Length * freq * (1 - freq)) / ((double)2 * Math.Sqrt(2 * valsWoTr.Length) * freq * ((double)1 - freq));
+
+            int[] sums = new int[vals.Length + 2];
+            sums[0] = sums[sums.Length - 1] = 0;
+            int accum = 0;
+            int zeroCount = 2;
+            for (int i = 1, j = 0; i < sums.Length - 1; ++i, ++j) {
+                accum += vals[j];
+                sums[i] = accum;
+                if (sums[i] == 0) zeroCount++;
+            }
+            int lValue = zeroCount - 1;
+
+            int[] dzetas = new int[18];
+            for (int i = -9; i < 10; ++i) {
+                if (i == 0) continue;
+                foreach (var sum in sums) {
+                    if (i == sum) dzetas[_dzToArr(i)]++;
+                }
+            }
+
+            double[] extendedStat = new double[18];
+            for (int i = 0; i < 18; ++i) {
+                extendedStat[i] = (double)Math.Abs(dzetas[i] - lValue) / Math.Sqrt((double)2 * lValue * (4 * Math.Abs(_arrToDz(i)) - 2));
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Частотный тест пройден");
+            if (statEqual > bound) {
+                sb.Append("Тест на последовательность одинаковых бит не пройден");
+            }
+            else {
+                sb.Append("Тест на последовательность одинаковых бит пройден");
+            }
+            foreach (var i in extendedStat) {
+                if (i > bound) {
+                    sb.Append("Расширенный тест на произвольные отклонения не пройден");
+                }
+            }
+            if (sb.Length == 2) {
+                sb.Append("Расширенный тест на произвольные отклонения пройден");
+            }
+
+            MessageBox.Show(sb.ToString());
         }
 
-        private int[] getVals(string str) {
+        private int _dzToArr(int pos) {
+            if (pos <= -1) return pos + 9;
+            else return pos + 8;
+        }
+
+        private int _arrToDz(int pos) {
+            if (pos <= 8) return pos - 9;
+            else return pos - 8;
+        }
+
+        private int[] getVals(string str, bool transform) {
             List<int> vals = new List<int>();
-            foreach (var i in str) {
+            for (int i = 0; i < str.Length; ++i) {
                 bool isDel = false;
                 foreach (var del in _delims) {
-                    if (i == del) {
+                    if (str[i] == del) {
                         isDel = true;
                         break;
                     }
                 }
-                if (!isDel && (i != '1' || i != '0'))
-                {
-                    // error
+                if (!isDel && str[i] != '1' && str[i] != '0') {
+                    MessageBox.Show($"Unexpected symbol at {i}");
+                    return vals.ToArray();
                 }
                 else {
-                    if (i == '1') vals.Add(1);
+                    if (str[i] == '1') vals.Add(1);
                     else vals.Add(0);
+                }
+            }
+            if (transform) {
+                for (int i = 0; i < vals.Count; ++i) {
+                    vals[i] = 2 * vals[i] - 1;
                 }
             }
             return vals.ToArray();
         }
 
-        public void stop()
-        {
+        public void stop() {
             _menu.Items.RemoveAt(_pos);
         }
     }
